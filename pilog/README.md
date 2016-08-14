4fb10b76e094451a9e6da83bab40f1dc85c249a0
marcheiligers <marc@madmimi.com>
2016-08-13 19:50:41 -0700

    Extract PILOG methods

---
+++ pilog/README.md
@@ -0,0 +1,3 @@
+# PILOG
+
+This is a set of library functions to send data to [my PILOG application](https://github.com/marcheiligers/pilog).
---
+++ pilog/pilog.py
@@ -0,0 +1,22 @@
+import os
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
+
+def post_to_pilog(data):
+  url = "https://pilog.herokuapp.com/logs"
+  headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+  usn, pwd = os.environ["PILOG_AUTH"].split(":")
+  auth = HTTPBasicAuth(usn, pwd)
+  content = { 'content': data })
+  resp = requests.post(url, data=json.dumps(content), headers=headers, auth=auth)
+  log_response(resp)
+  return resp
+
+def log_response(resp):
+  log(r.text)
+
+def log(message):
+  with open('pilog.log', 'a') as f:
+    f.write("{0}: {1}\n".format(datetime.now().isoformat(), message))
