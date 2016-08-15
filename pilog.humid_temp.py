453986872dd5dc784a4953607da6d70429417668
marcheiligers <marc@madmimi.com>
2016-08-14 19:03:42 -0700

    Erm, dumb dumb dumb... untesting

--- pilog.humid_temp.py
+++ pilog.humid_temp.py
@@ -1,6 +1,6 @@
 import socket
 from pilog import *
-# from dht11 import *
+from dht11 import *
 
-humidity, temperature = (19, 28) #read_humidity_and_temp()
+humidity, temperature = read_humidity_and_temp()
 post_weather(humidity, temperature)
